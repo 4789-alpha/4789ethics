633ad9b8411dbc8fac44c57112447053668eebb4
4789 <raphael.lauper@hyperwerk.ch>
2025-05-25 23:16:13 +0200

    Simplify OP-0 navigation

--- interface/op0-navigation.js
+++ interface/op0-navigation.js
@@ -13,13 +13,13 @@
     return lvl;
   }
   function insertNav(){
+    if(document.querySelector('nav')) return;
     const base = location.pathname.includes('/wings/') ? '../' : '';
     const navHtml =
       '<nav class="op0-nav">'+
       `<a href="${base}start.html">Start</a>`+
       `<a href="${base}bewertung.html">Bewertung</a>`+
       `<a href="${base}interface/ethicom.html">Ethicom</a>`+
-      `<a href="${base}interface/signup.html">Signup</a>`+
       '</nav>';
     document.body.insertAdjacentHTML('afterbegin', navHtml);
   }
