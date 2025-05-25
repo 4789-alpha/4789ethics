df8f5dbb384305183fde40582e27277464f2699e
4789 <raphael.lauper@hyperwerk.ch>
2025-05-25 20:20:54 +0200

    Add OP-0 navigation bar

---
+++ interface/op0-navigation.js
@@ -0,0 +1,29 @@
+(function(){
+  function getOpLevelFromUser(obj){
+    if(obj && typeof obj.op_level !== 'undefined'){ return parseInt(obj.op_level, 10); }
+    return null;
+  }
+  function getOpLevel(){
+    let lvl = getOpLevelFromUser(window.user);
+    if(lvl === null && typeof getStoredOpLevel === 'function'){
+      const stored = getStoredOpLevel();
+      if(stored){ lvl = parseInt(String(stored).replace('OP-',''), 10); }
+    }
+    if(Number.isNaN(lvl)) lvl = null;
+    return lvl;
+  }
+  function insertNav(){
+    const navHtml =
+      '<nav class="op0-nav">'+
+      '<a href="start.html">Start</a>'+
+      '<a href="bewertungen.html">Bewertungen</a>'+
+      '<a href="ethicom.html">Ethicom</a>'+
+      '<a href="signup.html">Signup</a>'+
+      '</nav>';
+    document.body.insertAdjacentHTML('afterbegin', navHtml);
+  }
+  document.addEventListener('DOMContentLoaded', function(){
+    const level = getOpLevel();
+    if(level === 0){ insertNav(); }
+  });
+})();
